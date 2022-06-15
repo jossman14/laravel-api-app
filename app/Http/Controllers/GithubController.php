a1faae228f3544b3e0e217d7b2e82018925bf1e1
Tanzilal Mustaqim <derekln14@gmail.com>
2022-06-16 06:45:20 +0700

    update github api

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Http;
 class GithubController extends Controller
 {
     public function getApi(){
-        $response = Http::withToken("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")->get("https://api.github.com/repos/jossman14/laravel-api-app");
+        $response = Http::withToken(env("GITHUB_KEY", ""))->get("https://api.github.com/repos/jossman14/laravel-api-app");
 
         return json_decode($response);
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(20)->create();
+        
     }
 }
